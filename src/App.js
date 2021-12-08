4a9e8215d3effc02353bc22989fe996579647dcc
mohit9019 <mohitchauhan2802@gmail.com>
2021-12-08 22:40:56 +0530

    'commit'

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React from "react";
 import Slot from "./Slot";
 import Data from "./Data";
 import Navbar from "./Navbar";
-import Footer from "./Footer";
-import 
+import Footer from "./Footer"; 
 
 
 function details(val){
@@ -28,9 +27,9 @@ const App=()=>{
   return(
     <> 
       <Navbar />
-      <Link to="/second">
+      {/* <Link to="/second"> */}
       <h3>second link</h3>
-      </Link>
+      {/* </Link> */}
       {Data.map(details)}
     
       <Footer />
